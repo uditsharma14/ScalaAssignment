993d6b48f03f605aea066142db66627e25c879ba
Udit Sharma <UditSharma14@gmail.com>
2017-01-04 02:05:25 +0530

    looping scala

---
+++ ScalaLoopingExamples/src/com/scala/loop/whileLoop.scala
@@ -0,0 +1,12 @@
+package com.scala.loop
+
+object whileLoop {
+  def main(args: Array[String]) {
+      var a = 10;
+      
+      // An infinite loop.
+      while( true ){
+         println( "Value of a: " + a );
+      }
+   }
+}
